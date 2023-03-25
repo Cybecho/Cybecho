514c7ebd2704d2f9b2cc3e865db38744ce60e26b
HelloZOOO <qnrwnd10119@gmail.com>
2023-03-25 05:44:44 +0000

    Automated commit Sat Mar 25 05:44:44 UTC 2023

--- randomImg.sh
+++ randomImg.sh
@@ -18,9 +18,8 @@ My profile picture is my selfie Steal X
 
 <a href=\"https://cybecho.notion.site/SBU-s-Archives-854ccd3338c2456a867956f26143998a\" target=\"_blank\"><img src=\"https://img.shields.io/badge/Portfolio-3030\" >
 <a href=\"https://www.instagram.com/junk_warrior_vintage/\" target=\"_blank\"><img src=\"https://img.shields.io/badge/@junk_warrir_vintage-E4405F?style=for- the-b\" >
-<a href=\"https://www.behance.net/thquddnr125654\" target=\"_blank\"><img src=\"https://img.shields.io/badge/Behance-1769FF?style=for-the- badge&logo=Behance&logo>
+<a href="https://www.behance.net/thquddnr125654" target="_blank"><img src="https://img.shields.io/badge/Behance-1769FF?style=for-the-badge&logo=Behance&logoColor=white"/></a>
 
-![](https://www.random-art.org/img/large/417819.jpg)
 
 <!--
 Markup Quick View Site
