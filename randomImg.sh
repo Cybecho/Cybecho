5175bd29b256cec1a0549f9c31ef94ea0b011ac6
HelloZOOO <qnrwnd10119@gmail.com>
2023-03-25 12:48:01 +0000

    Automated commit Sat Mar 25 12:48:01 UTC 2023

--- randomImg.sh
+++ randomImg.sh
@@ -3,7 +3,7 @@ source /home/thquddnr123/.bashrc
 cd /home/thquddnr123/AutoCommit/HelloZOOO/
 
 # 원본 README 복사
-cat README_origin.md > README.md
+cat /home/thquddnr123/AutoCommit/HelloZOOO/README_origin.md > /home/thquddnr123/AutoCommit/HelloZOOO/README.md
 
 # 랜덤한 난수를 생성하여 URL부분에 입력해주는 과정
 echo "<table>" > /home/thquddnr123/AutoCommit/HelloZOOO/table.html
@@ -13,10 +13,10 @@ for i in {1..3}; do
         rand_num=$(( $RANDOM % 2001 + 415500 ))
         echo "<td><img src='https://www.random-art.org/img/large/$rand_num.jpg'></td>" >> table.html
     done
-    echo "</tr>" >> table.html
+    echo "</tr>" >> /home/thquddnr123/AutoCommit/HelloZOOO/table.html
 done
-echo "</table>" >> table.html
+echo "</table>" >> /home/thquddnr123/AutoCommit/HelloZOOO/table.html
 
 # README.md 파일에 테이블 정보 붙여넣기
-echo " $(echo -e "<br/>") $(cat table.html)" >> README.md
+echo " $(echo -e "<br/>") $(cat table.html)" >> /home/thquddnr123/AutoCommit/HelloZOOO/README.md
 
